d3701b53ae4d51664d158e6375ab40b14e73f171
zongxiong.chen <czxczf@gmail.com>
2019-05-19 06:30:26 +0200

    add papi results

--- run_yahoo.sh
+++ run_yahoo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-./run.sh uk.ac.imperial.lsds.saber.experiments.benchmarks.yahoo.YahooBenchmarkApp 8 icache-miss &> target/tmp/icache-miss.txt
-./run.sh uk.ac.imperial.lsds.saber.experiments.benchmarks.yahoo.YahooBenchmarkApp 8 dcache-miss &> target/tmp/dcache-miss.txt
-./run.sh uk.ac.imperial.lsds.saber.experiments.benchmarks.yahoo.YahooBenchmarkApp 8 tcache-miss &> target/tmp/tcache-miss.txt
-./run.sh uk.ac.imperial.lsds.saber.experiments.benchmarks.yahoo.YahooBenchmarkApp 8 tlb         &> target/tmp/tlb.txt
-./run.sh uk.ac.imperial.lsds.saber.experiments.benchmarks.yahoo.YahooBenchmarkApp 8 branch      &> target/tmp/branch.txt
-./run.sh uk.ac.imperial.lsds.saber.experiments.benchmarks.yahoo.YahooBenchmarkApp 8 instr-cycle &> target/tmp/instr-cycle.txt
+./run.sh uk.ac.imperial.lsds.saber.experiments.benchmarks.yahoo.YahooBenchmarkApp 8 icache-miss &> papi-result/gpu4/icache-miss.txt
+./run.sh uk.ac.imperial.lsds.saber.experiments.benchmarks.yahoo.YahooBenchmarkApp 8 dcache-miss &> papi-result/gpu4/dcache-miss.txt
+./run.sh uk.ac.imperial.lsds.saber.experiments.benchmarks.yahoo.YahooBenchmarkApp 8 tcache-miss &> papi-result/gpu4/tcache-miss.txt
+./run.sh uk.ac.imperial.lsds.saber.experiments.benchmarks.yahoo.YahooBenchmarkApp 8 tlb         &> papi-result/gpu4/tlb.txt
+./run.sh uk.ac.imperial.lsds.saber.experiments.benchmarks.yahoo.YahooBenchmarkApp 8 branch      &> papi-result/gpu4/branch.txt
+./run.sh uk.ac.imperial.lsds.saber.experiments.benchmarks.yahoo.YahooBenchmarkApp 8 instr-cycle &> papi-result/gpu4/instr-cycle.txt
